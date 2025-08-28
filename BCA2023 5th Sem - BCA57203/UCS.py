2ae96db18e1bb9fed9875403b6c1ca534891d287
soumadipDey <soumadipdey63@gmail.com>
2025-08-28 12:17:33 +0530

    bug fixes to te UCS program

--- BCA2023 5th Sem - BCA57203/UCS.py
+++ BCA2023 5th Sem - BCA57203/UCS.py
@@ -9,6 +9,7 @@ def printPath(node, parent):
 def UCS(graph, start, goal_set, n_nodes):
     visited = [False] * n_nodes
     parent = [-1] * n_nodes
+    distances = [float('inf')] * n_nodes
     pq = PriorityQueue()
     pq.put((0,start))
 
@@ -23,25 +24,29 @@ def UCS(graph, start, goal_set, n_nodes):
             for neighbour, cost in graph[curr_node]:
                 if not visited[neighbour]:
                     new_dist = dist + cost
-                    parent[neighbour] = curr_node
-                    pq.put((new_dist,neighbour))
+                    if new_dist < distances[neighbour]:
+                        distances[neighbour] = new_dist
+                        parent[neighbour] = curr_node
+                        pq.put((new_dist,neighbour))
     
     return -1
 
 graph = {
-    0: [(1, 1), (2, 4)],
-    1: [(2, 2), (3, 5)],
-    2: [(4, 3)],
-    3: [(5, 2), (6, 1)],
-    4: [(6, 4), (3, 1)],
-    5: [],
-    6: [(7, 6)],
-    7: []
+    0: [(1, 2), (3, 5)],
+    1: [(2, 1), (4, 4)],
+    2: [(0, 1), (3, 1), (5, 3)],
+    3: [(1, 1), (4, 3), (5, 2), (6, 2), (7, 4), (8, 1)],
+    4: [(7, 2)],
+    5: [(6, 2)],
+    6: [(7, 1), (8, 2)],
+    7: [],
+    8: [(9, 4)],
+    9: []
 }
 
 start = 0
-goal_set = {5,7}
-n_nodes = 8
+goal_set = {7,9}
+n_nodes = len(graph)
 
 print(f"Running UCS on the given graph for Start: {start} and Goal Set: {goal_set}")
 print(f"\nResulting Cost: {UCS(graph,start,goal_set,n_nodes)}")
